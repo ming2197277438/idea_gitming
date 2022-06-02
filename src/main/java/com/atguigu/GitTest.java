225308293dc143d14a929e58fe4a49917d40e0be
sunming <2197277438@qq.com>
2022-06-02 09:50:04 +0800

    第二次更新

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -3,6 +3,7 @@ package com.atguigu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("我叫龚佩杰");
-        System.out.println("臭鱼烂虾");
+        System.out.println("0217班");
+        System.out.println("顶级大佬");
     }
 }
